23452ec04044599c84d66be50df26fa8c605df7b
Rupal Jain <jainrupal9727@gmail.com>
2024-10-05 13:31:37 -0700

    Updated kilo.cpp

--- kilo.cpp
+++ kilo.cpp
@@ -5,7 +5,7 @@ int main()
 {
     char c; // Character to store input
     // Reading one byte at a time from standard input
-    while (read(STDIN_FILENO, &c, 1) == 1)
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
     {
         // For now, we just loop until EOF (no specific action taken)
     }
