1167f0005dc925c396111277bd7781830e3bc81a
Rupal Jain <jainrupal9727@gmail.com>
2024-11-07 12:37:14 -0800

    Updated kilo.cpp

--- kilo.cpp
+++ kilo.cpp
@@ -21,6 +21,7 @@ struct editorConfig
 {
     int cx, cy;
     int rowoff;
+    int coloff;
     int screenrows;
     int screencols;
     int numrows;
@@ -272,10 +273,10 @@ void editorMoveCursor(int key)
         }
         break;
     case ARROW_RIGHT:
-        if (E.cx != E.screencols - 1)
-        {
+        // if (E.cx != E.screencols - 1)
+        // {
             E.cx++;
-        }
+        // }
         break;
     case ARROW_UP:
         if (E.cy != 0)
@@ -336,6 +337,11 @@ void editorScroll()
         E.rowoff = E.cy;
     if (E.cy >= E.rowoff + E.screenrows)
         E.rowoff = E.cy - E.screenrows + 1;
+
+    if (E.cx < E.coloff)
+        E.coloff = E.cx;
+    if (E.cx >= E.coloff + E.screencols)
+        E.coloff = E.cx - E.screencols + 1;
 }
 
 void editorDrawRows(std::string &ab)
@@ -370,10 +376,12 @@ void editorDrawRows(std::string &ab)
         }
         else
         {
-            int len = E.row[filerow].size;
+            int len = E.row[filerow].size - E.coloff;
+            if (len < 0)
+                len = 0;
             if (len > E.screencols)
                 len = E.screencols;
-            ab.append(E.row[filerow].chars, len);
+            ab.append(&E.row[filerow].chars[E.coloff], len);
         }
         ab.append("\x1b[K"); // clear lines as we draw
         if (y < E.screenrows - 1)
@@ -416,7 +424,7 @@ void editorRefreshScreen()
     ab.append("\x1b[H");
 
     char buf[32];
-    int welcomelen = snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+    int welcomelen = snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
     ab.append(buf);
 
     ab.append("\x1b[?25h"); // Hide the cursor
@@ -432,6 +440,7 @@ void initEditor()
     E.cx = 0;
     E.cy = 0;
     E.rowoff = 0;
+    E.coloff = 0;
     E.numrows = 0;
     E.row = nullptr;
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
