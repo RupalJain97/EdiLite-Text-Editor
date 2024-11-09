affa54b5891e8018b53d20e1b3235ff3e1bbdb28
Rupal Jain <jainrupal9727@gmail.com>
2024-11-08 16:20:59 -0800

    Updated kilo.cpp

--- kilo.cpp
+++ kilo.cpp
@@ -69,6 +69,7 @@ enum editorKey
 enum editorHighlight
 {
     HL_NORMAL = 0,
+    HL_COMMENT,
     HL_STRING,
     HL_NUMBER,
     HL_MATCH
@@ -78,6 +79,7 @@ struct editorSyntax
 {
     char *filetype;
     char **filematch;
+    char *singleline_comment_start;
     int flags;
 };
 
@@ -85,6 +87,7 @@ char *C_HL_extensions[] = {".c", ".h", ".cpp", NULL};
 struct editorSyntax HLDB[] = {
     {"c",
      C_HL_extensions,
+     "//",
      HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS},
 };
 
@@ -271,6 +274,8 @@ int editorSyntaxToColor(int hl)
 {
     switch (hl)
     {
+    case HL_COMMENT:
+        return 36; // Cyan
     case HL_STRING:
         return 35; // Magenta
     case HL_NUMBER:
@@ -296,6 +301,9 @@ void editorUpdateSyntax(erow *row)
     if (E.syntax == NULL)
         return;
 
+    char *scs = E.syntax->singleline_comment_start;
+    int scs_len = scs ? strlen(scs) : 0;
+
     int prev_sep = 1;
     int in_string = 0;
     int i = 0;
@@ -304,6 +312,15 @@ void editorUpdateSyntax(erow *row)
     {
         char c = row->render[i];
         unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+        if (scs_len && !in_string)
+        {
+            if (!strncmp(&row->render[i], scs, scs_len))
+            {
+                memset(&row->hl[i], HL_COMMENT, row->rsize - i);
+                break;
+            }
+        }
 
         if (E.syntax->flags & HL_HIGHLIGHT_STRINGS)
         {
