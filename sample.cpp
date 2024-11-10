85fb2af41bd464ad14702f6d1095491bbe5bc2db
Rupal Jain <jainrupal9727@gmail.com>
2024-11-09 20:11:09 -0800

    Update ediLite.cpp

--- sample.cpp
+++ sample.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 
-int main(){
-	for(int i=30;i<=37;i++){
-		std::cout <<"\033[" << i << "m " << i<< ": sample  \n";
+int main()
+{
+	std::cout << "Basic ANSI Colors (30-37):\n";
+	for (int i = 30; i <= 37; i++)
+	{
+		std::cout << "\033[" << i << "m" << i << ": sample text\033[0m\n";
 	}
-	std::cout<<"\n";
-	for(int i=90;i<=97;i++){
-		std::cout<<"\033[" << i << "m " << i<< ": sample \n";
+
+	std::cout << "\nHigh-Intensity ANSI Colors (90-97):\n";
+	for (int i = 90; i <= 97; i++)
+	{
+		std::cout << "\033[" << i << "m" << i << ": sample text\033[0m\n";
 	}
-	std::cout<<"\n";
+
+	std::cout << "\nExtended 256 Colors (38;5;0-255):\n";
+	for (int i = 0; i < 256; i++)
+	{
+		std::cout << "  \033[38;5;" << i << "m" << i << ": sample text\033[0m\t  ";
+		if ((i + 1) % 8 == 0)
+		{ // To format into columns
+			std::cout << "\n";
+		}
+	}
+
+	std::cout << "\n";
 	return 0;
 }
