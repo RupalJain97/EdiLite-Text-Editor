e70056fbb6cddf30c5b37454356fb4791af87feb
Rupal Jain <jainrupal9727@gmail.com>
2024-11-09 21:15:05 -0800

    Added colors

--- sample.cpp
+++ sample.cpp
@@ -2,17 +2,18 @@
 
 int main()
 {
-	std::cout << "Basic ANSI Colors (30-37):\n";
 	for (int i = 30; i <= 37; i++)
-	{
-		std::cout << "\033[" << i << "m" << i << ": sample text\033[0m\n";
-	}
+    {
+        std::cout << "\033[" << i << "m" << i << ": foreground text\033[0m   ";
+        std::cout << "\033[" << i + 10 << "m" << i + 10 << ": background text\033[0m\n";
+    }
 
-	std::cout << "\nHigh-Intensity ANSI Colors (90-97):\n";
-	for (int i = 90; i <= 97; i++)
-	{
-		std::cout << "\033[" << i << "m" << i << ": sample text\033[0m\n";
-	}
+    std::cout << "\nHigh-Intensity ANSI Colors (90-97) - Foreground and Background:\n";
+    for (int i = 90; i <= 97; i++)
+    {
+        std::cout << "\033[" << i << "m" << i << ": foreground text\033[0m   ";
+        std::cout << "\033[" << i + 10 << "m" << i + 10 << ": background text\033[0m\n";
+    }
 
 	std::cout << "\nExtended 256 Colors (38;5;0-255):\n";
 	for (int i = 0; i < 256; i++)
